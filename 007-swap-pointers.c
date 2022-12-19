c9e3512c308481f58c0441c329d43458cae88064
Marie Behzadi <mariebehzadi@gmail.com>
2022-12-19 18:48:37 +0330

    [EX007]: Swap Function | using pointer variables and outside of main function

---
+++ 007-swap-pointers.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void swap (int *ptr1, int *ptr2) {
+    int tmp;
+    tmp = *ptr1;
+    *ptr1 = *ptr2;
+    *ptr2 = tmp;
+
+}
+
+int main () {
+    int num1, num2;
+
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+
+    printf("Enter second number: ");
+    scanf("%d", &num2);
+
+    printf("Before Swap: num1 = %d and num2 = %d \n", num1, num2);
+    swap (&num1, &num2);
+    printf("After Swap: num1 = %d and num2 = %d \n", num1, num2);
+    
+    return 0;
+}
