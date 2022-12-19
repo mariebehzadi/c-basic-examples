b31b45fcab6a0f48225b51866c54c197aa4d5519
Marie Behzadi <mariebehzadi@gmail.com>
2022-12-19 17:57:53 +0330

    [EX005]: Data Types | check the size of different data types

---
+++ 005-data-types.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main () {
+    char char1;
+    printf("size of char is: %d \n", sizeof(char1));
+    printf("size of char is: %d \n", sizeof(char));
+    printf("------------------------------ \n");
+    
+    printf("size of 'a' is: %d \n", sizeof('a')); // this is a character
+    printf("size of \"a\" is: %d \n", sizeof("a")); // this is a string ending with \0
+    printf("------------------------------ \n");
+
+    int num1;
+    printf("size of num is: %d \n", sizeof(num1));
+    printf("size of int is: %d \n", sizeof(int));
+    printf("------------------------------ \n");
+
+    float num2;
+    printf("size of num is: %d \n", sizeof(num2));
+    printf("size of float is: %d \n", sizeof(float));
+    printf("------------------------------ \n");
+
+    double num3;
+    printf("size of num is: %d \n", sizeof(num3));
+    printf("size of double is: %d \n", sizeof(double));
+    printf("------------------------------ \n");
+
+    return 0;
+}
